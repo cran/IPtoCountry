181240e641f845c7e09e09dfc9ce7f76043c18d3
Ronald E. Robertson <robertson.ron@husky.neu.edu>
2016-07-22 01:31:05 +0000

    version 0.0.1

---
+++ R/data-documentation.R
@@ -0,0 +1,26 @@
+#' IP addresses
+#'
+#' A dataset containing 1000 randomly generated IP addresses
+#'
+#' @format A character vector with length 1000
+#' @source Generated using \code{\link{IP_generator}}
+"IPs"
+
+
+#' IP database - small
+#'
+#' A dataset containing 149,449 IP integer ranges and their corresponding country
+#' names and abbreviations
+#'
+#' Last updated: 06/06/2016
+#'
+#' @format A data.frame with four variables
+#' \describe{
+#'   \item{IPfrom}{Start of IP integer range}
+#'   \item{IPto}{End of IP integer range}
+#'   \item{Abrv}{Two-character country code based on ISO 3166.}
+#'   \item{Country}{Country name based on ISO 3166.}
+#' }
+#' @source \url{http://lite.ip2location.com}
+"ip2location.lite.db1"
+
